5ecf2fb0f1d120f17c6d7f0925afc7d0d1e9292b
Andrés Cañón M <andres.canon@iyu.com.co>
2021-03-28 12:45:47 -0500

    EmpresaModule

---
+++ src/app/empresa/empresa.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormularioComponent } from './formulario/formulario.component';
+
+
+
+@NgModule({
+  declarations: [FormularioComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmpresaModule { }
